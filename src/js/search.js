c77366b3a516d33b4d77554ebbf0577f8e623aba
Crisciano da Silva Botelho <crisciano.botelho@pampa.compasso>
2019-04-15 10:11:47 -0300

    SavePoint

--- src/js/search.js
+++ src/js/search.js
@@ -1,2 +1,17 @@
 var d = document;
 
+var inputSearch = d.querySelector('.search');
+var pacientes = d.querySelectorAll(".paciente");
+
+inputSearch.addEventListener('input', (event)=>{
+
+    if (this.value.length > 0 ) {
+        pacientes.forEach( paciente =>{
+            var pacienteNome = paciente.querySelector('.info-nome').textContent;
+            console.log(pacienteNome);
+        })
+        
+    }
+    
+
+});
