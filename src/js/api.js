3e3d2a862c372f543844dece2b147c39de6495ad
crisciano <criscianotsi@gmail.com>
2019-04-15 11:11:47 -0300

    SavePoint

---
+++ src/js/api.js
@@ -0,0 +1,40 @@
+var d = document;
+var urlApi = "https://api-pacientes.herokuapp.com/pacientes";
+var __table = d.querySelector('#tabela-pacientes');
+
+fetch(urlApi)
+    .then(res => res.json())
+    .then((res)=>{
+        res.map( paciente =>{ CreateTemplate(paciente) })
+    })
+
+// recever obj
+function CreateTemplate(paciente){
+    var tr = CreateElement('tr');
+    tr.classList.add('paciente');
+
+    var tdNome = CreateRow(paciente.nome, 'info-nome');
+    var tdPeso = CreateRow(paciente.peso, 'info-peso');
+    var tdAltura = CreateRow(paciente.altura, 'info-altura');
+    var tdGordura = CreateRow(paciente.gordura, 'info-gordura');
+    var tdImc = CreateRow(paciente.imc, 'info-imc');
+
+    tr.appendChild(tdNome);
+    tr.appendChild(tdPeso);
+    tr.appendChild(tdAltura);
+    tr.appendChild(tdGordura);
+    tr.appendChild(tdImc);
+
+    __table.appendChild(tr);
+}
+
+function CreateRow(value, className){
+    var td = CreateElement('td');
+    td.classList.add(className);
+    td.innerHTML = value;
+    return td;
+}
+
+function CreateElement(tag){
+    return d.createElement(tag);
+}
