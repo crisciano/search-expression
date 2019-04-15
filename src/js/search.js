aac17c1521591305ee0fd4918c9b8ec2a5de8a04
crisciano <criscianotsi@gmail.com>
2019-04-15 10:25:44 -0300

    SavePoint

--- src/js/search.js
+++ src/js/search.js
@@ -1,14 +1,16 @@
 var d = document;
 
 var inputSearch = d.querySelector('.search');
-var pacientes = d.querySelectorAll(".paciente");
 
-inputSearch.addEventListener("input", function(event){
-
+inputSearch.addEventListener("input", function(){
+    var pacientes = d.querySelectorAll(".paciente");
+    
     if (this.value.length > 0 ) {
         pacientes.forEach( paciente =>{
             var pacienteNome = paciente.querySelector('.info-nome').textContent;
-            // console.log(pacienteNome);
-        })
-    }
+            var expression = new RegExp(this.value, "i");
+            if(expression.test(pacienteNome)) paciente.classList.remove('none');
+            else paciente.classList.add('none'); 
+        });
+    }else  pacientes.forEach( paciente => paciente.classList.remove("none") );
 });
