4f571088c20d00e6ea3cf6f2ac0e79a6ee7e3370
crisciano <criscianotsi@gmail.com>
2019-04-18 10:37:04 -0300

    Order modifed.

--- src/js/order.js
+++ src/js/order.js
@@ -1,13 +1,6 @@
 var d = document;
 
-// 0 - nome     - string
-// 1 - peso     - number
-// 2 - altura   - number
-// 3 - gordura  - number
-// 4 - imc      - number
-
 function order(line, type){
-    // console.log(`${line} - ${type}`);
     
     var tbody = d.querySelector('#tabela-pacientes');
     var trs = tbody.querySelectorAll('tr');
@@ -18,18 +11,8 @@ function order(line, type){
         arr.push(tds[line])
         if(type == "string") reorders = orderString(arr);
         if(type == "number") reorders = orderNumber(arr);
-        // reorders = arr.sort((a,b)=>{ 
-        //     // if(type == "string"){
-        //     //     console.log(type);
-        //     //     if(a.innerHTML < b.innerHTML) { return -1; }
-        //     //     if(a.innerHTML > b.innerHTML) { return 1; }
-        //     //     return 0;
-        //     // }else if( type == 'number' ) {
-        //     //     console.log(type);
-        //         a.innerHTML - b.innerHTML
-        //     // }
-        // });
     })
+
     reorders.map(element=>{
         var tr = element.parentNode;
         tbody.appendChild(tr);
